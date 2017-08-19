a0e8be50f798f4d86668990fa03c42609364efa0
viliussepstas <31040628+viliussepstas@users.noreply.github.com>
2017-08-19 22:35:43 +0200

    vel
    vel

--- test5.py
+++ test5.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 path_to_chromedriver = '/Users/Vilius/Desktop/python/chromedriver'
 driver = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://jobs.theguardian.com/jobs/finance-and-accounting/#browsing'
-post_list = driver.find_elements_by_xpath('''//*[@id="main"]/div/div/div[1]/div[1]/div/div[2]/div[2]''')
 mainurl = driver.current_url
 next_page = driver.find_element_by_xpath('''//*[@id="main"]/div/div/div[2]/div[9]/ul/li[7]/a/i''')
 
@@ -14,8 +16,8 @@ def get_headers(driver):
     for header in header_list:
 	    driver.get(header.get_attribute("href"))
 
-    if driver.find_elements_by_xpath('''//*[@id="main"]/div/div/div[1]/div[1]/h1'''):
-    print link.text, driver.find_elements_by_xpath('''//*[@id="main"]/div/div/div[1]/div[1]/h1''').text
+    if driver.find_elements_by_xpath('//h1'):
+    print driver.find_elements_by_xpath('//h1').text
     else
     print "null"
-    driver.get(url)
+    driver.get(mainurl)
