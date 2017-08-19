3b7a66deab1e4cc3acdd7f647828eb2208a8dd8a
viliussepstas <31040628+viliussepstas@users.noreply.github.com>
2017-08-19 23:02:19 +0200

    new
    new

--- test5.py
+++ test5.py
@@ -6,18 +6,17 @@ from selenium.webdriver.common.by import By
 path_to_chromedriver = '/Users/Vilius/Desktop/python/chromedriver'
 driver = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://jobs.theguardian.com/jobs/finance-and-accounting/#browsing'
-mainurl = driver.current_url
-next_page = driver.find_element_by_xpath('''//*[@id="main"]/div/div/div[2]/div[9]/ul/li[7]/a/i''')
+driver.get(url)
 
-def get_headers(driver):
-    url = 'https://jobs.theguardian.com/jobs/finance-and-accounting/#browsing'
+def get_headers(driver, header_list):
+
+    mainurl = driver.current_url
     header_list = driver.find_elements_by_class_name('lister__view-details')
 
     for header in header_list:
 	    driver.get(header.get_attribute("href"))
 
     if driver.find_elements_by_xpath('//h1'):
-    print driver.find_elements_by_xpath('//h1').text
-    else
-    print "null"
-    driver.get(mainurl)
+        print header_list, driver.find_elements_by_xpath('//h1').text
+
+        driver.get(mainurl)
