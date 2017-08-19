1d986af2502054a09b8bc9c002d2e989974bebc3
vilius sepstas <31040628+viliussepstas@users.noreply.github.com>
2017-08-19 21:40:06 +0200

    Add files via upload

--- test5.py
+++ test5.py
@@ -5,12 +5,17 @@ driver = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://jobs.theguardian.com/jobs/finance-and-accounting/#browsing'
 post_list = driver.find_elements_by_xpath('''//*[@id="main"]/div/div/div[1]/div[1]/div/div[2]/div[2]''')
 driver.get(url)
-header_list = driver.find_elements_by_class_name('lister__view-details')
 next_page = driver.find_element_by_xpath('''//*[@id="main"]/div/div/div[2]/div[9]/ul/li[7]/a/i''')
 
 def get_headers(driver):
-    for header in header_list:
-        print [cell.text for cell in header.find_elements_by_class_name('lister__view-details')
+    url = 'https://jobs.theguardian.com/jobs/finance-and-accounting/#browsing'
+    header_list = driver.find_elements_by_class_name('lister__view-details')
 
-while True:
-    get_headers(driver)
+    for header in header_list:
+	    driver.get(header.get_attribute("href"))
+
+    if driver.find_elements_by_class_name('''grid-item three-fifths portable-one-whole palm-one-half'''):
+    print driver.find_elements_by_class_name('''grid-item three-fifths portable-one-whole palm-one-half''').text
+
+
+    driver.get(url)
