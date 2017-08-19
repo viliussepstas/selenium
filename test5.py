7f88c051ec76247ab8433e88d05be50c69ea03f4
viliussepstas <31040628+viliussepstas@users.noreply.github.com>
2017-08-19 21:57:13 +0200

    test2
    test2 description

--- test5.py
+++ test5.py
@@ -4,7 +4,7 @@ path_to_chromedriver = '/Users/Vilius/Desktop/python/chromedriver'
 driver = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://jobs.theguardian.com/jobs/finance-and-accounting/#browsing'
 post_list = driver.find_elements_by_xpath('''//*[@id="main"]/div/div/div[1]/div[1]/div/div[2]/div[2]''')
-driver.get(url)
+mainurl = driver.current_url
 next_page = driver.find_element_by_xpath('''//*[@id="main"]/div/div/div[2]/div[9]/ul/li[7]/a/i''')
 
 def get_headers(driver):
@@ -14,8 +14,8 @@ def get_headers(driver):
     for header in header_list:
 	    driver.get(header.get_attribute("href"))
 
-    if driver.find_elements_by_class_name('''grid-item three-fifths portable-one-whole palm-one-half'''):
-    print driver.find_elements_by_class_name('''grid-item three-fifths portable-one-whole palm-one-half''').text
-
-
+    if driver.find_elements_by_xpath('''//*[@id="main"]/div/div/div[1]/div[1]/h1'''):
+    print link.text, driver.find_elements_by_xpath('''//*[@id="main"]/div/div/div[1]/div[1]/h1''').text
+    else
+    print "null"
     driver.get(url)
